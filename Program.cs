d9ef2cfd2b12d35e8dcbb4a1047be5c8695c6082
Lang Hall <Lang Hall@ETERNAL-SPRING>
2024-02-05 19:43:46 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Iteration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //int total = 0;
+            //for (int i = 0;i < 7; i++)
+            //{
+            //    int maxtemp = Convert.ToInt32(Console.ReadLine());
+            //    total += maxtemp;
+            //}
+            //Console.WriteLine($"the average temperature this week is {total/7}");
+
+            //bool email = false;
+            //while (email == false) 
+            //{
+            //    Console.WriteLine("enter email");
+            //    string useremail = Console.ReadLine();
+            //    foreach (char c in useremail)
+            //    {
+            //        if (c == '@')
+            //            email = true;
+
+            //    }
+            //}
+            //Console.WriteLine("email valid");
+            bool email = false;
+            do
+            {
+                Console.WriteLine("enter email");
+                string useremail = Console.ReadLine();
+                foreach (char c in useremail)
+                {
+                    if (c == '@')
+                        email = true;
+
+                }
+            }while (email == false);
+            Console.WriteLine("email valid");
+        }
+    }
+}
